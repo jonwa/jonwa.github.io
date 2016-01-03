964e766ac2a625f443e4d9456299145ff0c602fb
jonwa <ejfwahlstrom@gmail.com>
2016-01-03 15:47:49 +0100

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,32 @@
+require.config({  
+  paths: {
+    'jquery': 'vendor/jquery/dist/jquery',
+    'underscore': 'vendor/underscore/underscore',
+    'backbone': 'vendor/backbone/backbone',
+    'backbone.babysitter': 'vendor/backbone.babysitter/lib/backbone.babysitter.js',
+    'backbone.wreqr': 'vendor/backbone.wreqr/lib/backbone.wreqr.js',
+	'backbone.radio': 'vendor/backbone.radio/build/backbone.radio.min',
+    'backbone.marionette': 'vendor/backbone.marionette/lib/core/backbone.marionette.js',
+	'handlebars': 'vendor/handlebars/handlebars',
+    'text': 'vendor/text/text',
+    'templates': '../templates'
+  },
+  shim: {
+    underscore: {
+        exports: '_'
+    },
+    backbone: {
+        exports: 'Backbone',
+        deps: ['jquery', 'underscore']
+    },
+    marionette: {
+        exports: 'Backbone.Marionette',
+        deps: ['backbone']
+    }
+  },
+  deps: ['jquery', 'underscore']
+});
+
+require(['views/app'], function(AppView) {  
+  new AppView;
+});
