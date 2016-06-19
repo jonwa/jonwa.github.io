81c5058438e4b6251b57abebc23a012cd308b10a
jonwa <jonwahlstrom@hotmail.com>
2016-06-20 00:22:18 +0200

    Updates variables

--- app/scripts/views/intro.js
+++ app/scripts/views/intro.js
@@ -1,6 +1,6 @@
 define(['jquery', 'underscore', 'backbone', 'backbone.marionette', 'handlebars', 'text!templates/intro.html'],
 function($, _, Backbone, Marionette, Handlebars, template){
     return Marionette.ItemView.extend({
-        template: Handlebars.compile(template),
+        template: Handlebars.compile(template)
     });
 });
--- app/scripts/views/projects.js
+++ app/scripts/views/projects.js
@@ -1,6 +1,6 @@
 define(['jquery', 'underscore', 'backbone', 'backbone.marionette', 'handlebars', 'text!templates/projects.html'],
     function($, _, Backbone, Marionette, Handlebars, template){
         return Marionette.ItemView.extend({
-            template: Handlebars.compile(template),
+            template: Handlebars.compile(template)
         });
     });
